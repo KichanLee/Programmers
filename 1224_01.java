afe86a53e9012ef51ff3211b83abd5285d6c92d9
KichanLee <72385451+KichanLee@users.noreply.github.com>
2022-12-27 11:29:57 +0900

    배열 슬라이스
    배열 슬라이스

---
+++ 1224_01.java
@@ -0,0 +1,15 @@
+
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        int[] answer = new int[(int)(right-left)+1];
+
+        int idx = 0;
+        for(long i=left; i<=right; i++){
+            int r = (int)(i/n)+1;
+            int c = (int)(i%n)+1;
+            answer[idx++] = r>=c?r:c;
+        }
+
+        return answer;
+    }
+}
